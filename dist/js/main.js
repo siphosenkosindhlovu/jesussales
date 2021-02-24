53a6424d0707ac6aa069f1428084aa2ca3d9fd71
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-02-24 05:55:41 +0200

    Clean dist

--- dist/js/main.js
+++
@@ -1,5 +0,0 @@
-/*! project-name v0.0.1 | (c) 2021 Siphosenkosi Ndhlovu | MIT License | https://github.com/siphosenkosindhlovu/gulp-boilerplate.git */
-document.addEventListener('click', (function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}), false);
